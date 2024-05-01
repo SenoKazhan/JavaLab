98f43c1ec984dc2c7b4ec596122869b2ebb3d39e
KSB <SenoKarazhan@yandex.by>
2024-05-01 11:39:20 +0300

    HTML test

--- src/main/java/com/projects/countrycode/controller/ControllerPhone.java
+++ src/main/java/com/projects/countrycode/controller/ControllerPhone.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +27,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 /** The type Controller phone. */
-@CrossOrigin
 @RestController
 @RequestMapping("/api/countries")
 public class ControllerPhone {
